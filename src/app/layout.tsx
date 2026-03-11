709dedcb2c5b9d948fbf8e02c5cf8dafc08258fa
Naoto Matsunaga <naoto.m.dev@gmail.com>
2026-03-11 20:27:01 +0900

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Naoto | Portfolio",
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
